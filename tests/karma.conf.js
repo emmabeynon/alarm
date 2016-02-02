6237b045787b8be225291eda9bbb516ab3a9a003
emmabeynon <emma.beynon@gmail.com>
2016-02-02 17:00:03 +0000

    Changed Karma browser to PhantomJS

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '"tests/**/*.js"'
+      'js/**/*.js',
+      'tests/**/*.js'
     ],
 
 
@@ -55,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
