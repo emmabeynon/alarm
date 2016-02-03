1650ba49fbc54f678974c77f70ad82257a9a4676
emmabeynon <emma.beynon@gmail.com>
2016-02-03 09:04:14 +0000

    Edited tab-account.html to display settings options.  Added feature spec file

---
+++ tests/e2e/alarmFeature.js
@@ -0,0 +1,21 @@
+describe('Alarm', function() {
+  it('has a title', function() {
+    browser.get('http://localhost:8100');
+    expect(browser.getTitle()).toEqual('Alarm');
+  });
+
+  // As a commuter
+  // So that the right alarm time is calculated for me
+  // I would like to specify my journey starting point
+  describe('User config', function() {
+    var settingsTab = element(by.css('.ion-ios-gear-outline'));
+    var startingPoint = element(by.id('starting-point'));
+    var startingPostcode = element(by.id('starting-postcode'));
+
+    it('allows user to enter their journey start postcode', function() {
+      settingsTab.click();
+      startingPoint.click();
+      startingPostcode.sendKeys('E1 6LT');
+    });
+  });
+});
