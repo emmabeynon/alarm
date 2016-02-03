5cd936531f454c0a9fc017d8aa0cfc2278b6be11
emmabeynon <emma.beynon@gmail.com>
2016-02-03 10:33:39 +0000

    Created SettingsCtrl controller

--- tests/e2e/alarmFeature.js
+++ tests/e2e/alarmFeature.js
@@ -9,13 +9,14 @@ describe('Alarm', function() {
   // I would like to specify my journey starting point
   describe('User config', function() {
     var settingsTab = element(by.css('.ion-ios-gear-outline'));
-    var startingPoint = element(by.id('starting-point'));
-    var startingPostcode = element(by.id('starting-postcode'));
+    var startingPoint = element(by.model('settingsCtrl.startingPoint'));
+    var saveButton = element(by.css('.button-positive'));
 
     it('allows user to enter their journey start postcode', function() {
       settingsTab.click();
-      startingPoint.click();
-      startingPostcode.sendKeys('E1 6LT');
+      startingPoint.sendKeys('E1 6LT');
+      saveButton.click();
+      expect(startingPoint.getAttribute('autofocus')).toContain('E1 6LT');
     });
   });
 });
